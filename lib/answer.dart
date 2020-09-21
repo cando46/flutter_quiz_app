dcdbef9cee491ae0487c1968927cb2fc27df2ac2
Candogan Ozdemir <ozdemir.candogan@gmail.com>
2020-09-21 18:52:50 +0300

    Answe and Question classes added

---
+++ lib/answer.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+
+  Answer(this.selectHandler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: RaisedButton(
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text("A"),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
